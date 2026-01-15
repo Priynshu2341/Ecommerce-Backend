dde9ef3f33dcc26379d587a540f22cfb4ea667a1
Priynshu2341 <priyanshukushwaha26@gmail.com>
2026-01-16 01:57:36 +0530

    added Cart Controller

---
+++ src/main/java/com/example/ecommerce/controller/CartController.java
@@ -0,0 +1,53 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.dto.CartDTO;
+import com.example.ecommerce.service.CartItemService;
+import com.example.ecommerce.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+    private final CartItemService cartItemService;
+
+
+    @GetMapping("/{customerId}")
+    public CartDTO getCart(@PathVariable Integer customerId) {
+        return cartService.getCart(customerId);
+    }
+
+
+    @PostMapping("/{customerId}/items")
+    public CartDTO addToCart(
+            @PathVariable Integer customerId,
+            @RequestParam UUID productId,
+            @RequestParam int quantity
+    ) {
+        return cartService.addToCart(customerId, productId, quantity);
+    }
+
+
+    @PutMapping("/{customerId}/items/{productId}")
+    public CartDTO updateCartItem(
+            @PathVariable Integer customerId,
+            @PathVariable UUID productId,
+            @RequestParam int quantity
+    ) {
+        return cartItemService.updateCartItem(customerId, productId, quantity);
+    }
+
+
+    @DeleteMapping("/{customerId}/items/{productId}")
+    public CartDTO removeCartItem(
+            @PathVariable Integer customerId,
+            @PathVariable UUID productId
+    ) {
+        return cartItemService.removeCartItem(customerId, productId);
+    }
+}
